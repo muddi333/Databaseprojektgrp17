ed48a0ade8d894108c5e8da6db3c50a29607caa0
Mourhaf <Mourhaf@192.168.0.16>
2015-05-05 04:49:31 +0200

    test snart færdig

--- src/test01917/TestDAO.java
+++ src/test01917/TestDAO.java
@@ -12,10 +12,16 @@ import connector01917.Connector;
 import daoimpl01917.MySQLProduktBatchDAO;
 import daoimpl01917.MySQLProduktBatchKompDAO;
 import daoimpl01917.MySQLRaavareBatchDAO;
+import daoimpl01917.MySQLRaavareDAO;
+import daoimpl01917.MySQLReceptDAO;
+import daoimpl01917.MySQLReceptKompDAO;
 import daointerfaces01917.DALException;
 import dto01917.ProduktBatchDTO;
 import dto01917.ProduktBatchKompDTO;
 import dto01917.RaavareBatchDTO;
+import dto01917.RaavareDTO;
+import dto01917.ReceptDTO;
+import dto01917.ReceptKompDTO;
 
 public class TestDAO {
 
@@ -63,7 +69,7 @@ public class TestDAO {
 	//		}
 	/**--------------------------------------------------------------------------------------------*/
 	MySQLProduktBatchKompDAO prbk = new MySQLProduktBatchKompDAO();
-	
+
 	@Test
 	public void testgetProduktBatchKomp() throws DALException{
 		ProduktBatchKompDTO prbkOBJ = prbk.getProduktBatchKomp(4, 7);
@@ -104,7 +110,7 @@ public class TestDAO {
 	//	}
 	/**--------------------------------------------------------------------------------------------*/
 	MySQLRaavareBatchDAO rvb = new MySQLRaavareBatchDAO();
-	
+
 	@Test
 	public void testgetRaavareBatch() throws DALException{
 		RaavareBatchDTO rvbOBJ = rvb.getRaavareBatch(7);
@@ -122,9 +128,100 @@ public class TestDAO {
 		List<RaavareBatchDTO> rvbOBJ = rvb.getRaavareBatchList(5);
 		assertEquals(rvbOBJ.size(), 2);
 	}
+	//	@Test
+	//	public void testcreateRaavareBatch() throws DALException{
+	//		RaavareBatchDTO test;
+	//		rvb.createRaavareBatch(test = new RaavareBatchDTO(8, 8, 200.0));
+	//		RaavareBatchDTO rvbOBJ = rvb.getRaavareBatch(8);
+	//		assertEquals(8, rvbOBJ.getRbId());
+	//		assertEquals(200.0, rvbOBJ.getMaengde(),0.0);
+	//		assertEquals(6, rvbOBJ.getRbId());
+	//	}
+	//	@Test
+	//	public void testupdateRaavareBatch() throws DALException{
+	//		RaavareBatchDTO test;
+	//		rvb.updateRaavareBatch(test = new RaavareBatchDTO(8, 7, 100.0));
+	//		RaavareBatchDTO rvbOBJ = rvb.getRaavareBatch(8);
+	//		assertEquals(7, rvbOBJ.getRaavareId());
+	//		assertEquals(100.0, rvbOBJ.getMaengde(),0.0);
+	//	}
+	/**--------------------------------------------------------------------------------------------*/
+	MySQLRaavareDAO rv = new MySQLRaavareDAO();
+
+	@Test
+	public void testgetRaavare() throws DALException{
+		RaavareDTO rvOBJ = rv.getRaavare(7);
+		assertEquals("champignon", rvOBJ.getRaavareNavn());
+		assertEquals("Igloo Frostvarer", rvOBJ.getLeverandoer());
+		assertEquals(7, rvOBJ.getRaavareId());
+	}
+	@Test
+	public void testgetRaavareList() throws DALException{
+		List<RaavareDTO> rvOBJ = rv.getRaavareList();
+		assertEquals(rvOBJ.size(), 7);
+	}
+	//	@Test
+	//	public void testcreateRaavare() throws DALException{
+	//		RaavareDTO test;
+	//		rv.createRaavare(test = new RaavareDTO(8, "fisk", "Bornholm"));
+	//		RaavareDTO rvOBJ = rv.getRaavare(8);
+	//		assertEquals("fisk", rvOBJ.getRaavareNavn());
+	//		assertEquals("Bornholm", rvOBJ.getLeverandoer());
+	//		assertEquals(8, rvOBJ.getRaavareId());
+	//	}
+	//	@Test
+	//	public void testupdateRaavare() throws DALException{
+	//		RaavareDTO test;
+	//		rv.updateRaavare(test = new RaavareDTO(8, "tun", "Middelhavet"));
+	//		RaavareDTO rvOBJ = rv.getRaavare(8);
+	//		assertEquals("tun", rvOBJ.getRaavareNavn());
+	//		assertEquals("Middelhavet", rvOBJ.getLeverandoer());
+	//		assertEquals(8, rvOBJ.getRaavareId());
+	//	}
+	/**--------------------------------------------------------------------------------------------*/
+	MySQLReceptDAO rc = new MySQLReceptDAO();
+
+	@Test
+	public void testgetRecept() throws DALException{
+		ReceptDTO rcOBJ = rc.getRecept(3);
+		assertEquals("capricciosa", rcOBJ.getReceptNavn());
+		assertEquals(3, rcOBJ.getReceptId());
+	}
+	@Test
+	public void testgetReceptList() throws DALException{
+		List<ReceptDTO> rcOBJ = rc.getReceptList();
+		assertEquals(rcOBJ.size(), 3);
+	}
+	//	@Test
+	//	public void testcreateRecept() throws DALException{
+	//		ReceptDTO test;
+	//		rc.createRecept(test = new ReceptDTO(4, "saras"));
+	//		ReceptDTO rcOBJ = rc.getRecept(4);
+	//		assertEquals("saras", rcOBJ.getReceptNavn());
+	//		assertEquals(4, rcOBJ.getReceptId());
+	//	}
+	//	@Test
+	//	public void testupdateRecept() throws DALException{
+	//		ReceptDTO test;
+	//		rc.updateRecept(test = new ReceptDTO(4, "manakish"));
+	//		ReceptDTO rcOBJ = rc.getRecept(4);
+	//		assertEquals("manakish", rcOBJ.getReceptNavn());
+	//		assertEquals(4, rcOBJ.getReceptId());
+	//	}
+	/**--------------------------------------------------------------------------------------------*/
+	MySQLReceptKompDAO rck = new MySQLReceptKompDAO();
 	
-	
-	
-	
+	@Test
+	public void testgetReceptKomp() throws DALException{
+		ReceptKompDTO rckOBJ = rck.getReceptKomp(3, 7);
+		assertEquals(1.0, rckOBJ.getNomNetto(),0.0);
+		assertEquals(0.1, rckOBJ.getTolerance(),0.0);
+		assertEquals(7, rckOBJ.getRaavareId());
+		assertEquals(3, rckOBJ.getReceptId());
+	}
+	@Test
+	public void testgetReceptKompList() throws DALException{
+		
+	}
 	
 }
